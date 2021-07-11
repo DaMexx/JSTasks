2d03ce3944d9339ceb2d5b68cacd5e73564e0e0b
Maxim <haron9373@gmail.com>
2021-07-11 16:16:38 +0300

    Fixed the first problem. Was the wrong condition.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //1) Написать рекурсивную функцию вычисления чисел Фибоначчи. На вход функции подавать длину конечного массива чисел.
+//неверное решение условия
 function fib(n) {
   if (n <= 1) {
-    return n
+    return n;
   } else {
     return fib(n - 1) + fib(n - 2);
   }
 }
 fib();
+
+
+//решение через возврат массива
+// let arrFib = [0, 1, 1];
+function fib(n) {
+  let arrFib = [0,1];
+  for (let i = 2; i <= n; i++) {
+    let firstNum = i-1;
+    let secondNum = i-2;
+    arrFib.push(firstNum + secondNum)
+  }
+  return arrFib
+}
+console.log(fib(7))
 
 //2) Функция. Принимает массив строк. Должна вернуть массив результатов проверки двух строк. Если у одной строки с последующей первый и последний символы, то true. Например ["asd", "afffd", "cc", "kk"]. Для такого массива функция должна вернуть [true, false, false]
 
@@ -20,7 +35,7 @@ function makeNewArr(arr) {
     if (firstValue[0] == secondValue[0] && firstValue.slice(-1) == secondValue.slice(-1)) {
       result.push(true);
     } else {
-      result.push(false)
+      result.push(false);
     }
   }
 
