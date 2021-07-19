a137dbb61081da39dde95d7b1b568d8008adbec5
Maxim Misuychenko <haron9373@gmail.com>
2021-07-19 15:36:39 +0300

    find mistake

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ function fib(n) {
     return [1];
   }
 
-  let arrFib = [1, 1];
+  let arrFib = [0, 1];
 
   //исправил значение аргументов, тк изначально указал просто число, а не позицию в массиве
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     let firstNum = arrFib[i - 1];
     let secondNum = arrFib[i - 2];
     arrFib.push(firstNum + secondNum)
@@ -31,7 +31,7 @@ function fib(n) {
 
   return arrFib;
 }
-fib(7);
+console.log(fib(7));
 
 
 //2) Функция. Принимает массив строк. Должна вернуть массив результатов проверки двух строк. Если у одной строки с последующей первый и последний символы, то true. Например ["asd", "afffd", "cc", "kk"]. Для такого массива функция должна вернуть [true, false, false]
@@ -65,30 +65,114 @@ matrix(3)
 8 9 4
 7 6 5*/
 
-//Промежуточное решение
 
-function makeArr(size) {
-  const arr = [];
+//создание новой матрицы
+function makeMatrix(n) {
+  let result = new Array(n).fill().map(() => Array(n).fill(''));
 
-  for (let i = 0; i < size; i++) {
-    arr.push(new Array(size).fill());
-  }
-  let start = 0
-  let x = 0;
-  let y = 0;
-  let n = size -1;
-  for (let i = 1; i <= size * size; i++) {
-   
-    arr[y][x] = i;
-
-    if (x < n) {//    0.0 0.1 
-      x++;
-    } else if (y < n) {
-      y++;    // 0.2 1.2 
-    } 
+  for (i = 0; i <= result.length; i++) {
+    if (Array.isArray(result[i])) {
+      result[i]/*.fill(7)*/
+    }
   }
 
-return arr
+  return result
+}
+
+
+console.log(makeMatrix(3));
+
+let arr = [];
+arr.fill();
+
+
+
+let arr = new Array(3)
+
+
+//черновик
+// let arr = new Array(3)
+// let arrO = new Array(3).fill('')
+// let arr1 = new Array(3).fill('')
+// let arr2 = new Array(3).fill('')
+// arr[0] = arrO
+// arr[1] = arrO
+// arr[2] = arrO
+// for (let i = 1; i <= arr[0].length; i++) {
+//   arr[0][i - 1] = i
+// }
+// for (let i = 1; i <= arr[1].length; i++) {
+//   arr[1][i - 1] = i
+// }
+// for (let i = 1; i <= arr[2].length; i++) {
+//   arr[2][i - 1] = i
+// }
+// console.log(arr)
+
+
+
+
+
+
+//еще способ задать массив
+// function newArr(n) {
+
+//   let arr = new Array(n);
+//   let arrN = new Array(n);
+//   let count = 0
+//   for (let i = 1; i <= arr.length; i++) {
+//     arr[i - 1] = arrN;
+//   }
+
+//   return arr
+// }
+// console.log(newArr(5))
+
+
+function makeArr(length) {
+  let arr = [];
+  for (let i = 0; i <= length; i++) {
+    return arr[i];
+
+  }
+  return arr
+}
+console.log(makeArr(5))
+
+
+
+function newArr(n) {
+
+  let arr = new Array(n);
+  let arrN = new Array(n);
+  let count = 0
+  for (let i = 1; i <= arr.length; i++) {
+    arr[i - 1] = arrN;
+  }
+
+  return arr
+}
+console.log(newArr(3))
+
+
+
+
+
+function makeArr(n) {
+  let = arr = [];
+
+  for (let i = 0; i < n; i++) {
+    arr[i] = Array(n);
+  } 
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[0][i++] = i
+  } 
+
+
+    // arr[1][arr[1].length-1]=i++
+
+  return arr
 }
 
 console.log(makeArr(3))
